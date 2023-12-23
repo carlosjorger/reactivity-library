0c8c6cee9e112afd8cd8206d278365c220225c9b
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-12-23 16:46:08 -0500

    ✨ feat: implement createEffect

--- src/reactivity-state.test.ts
+++ src/reactivity-state.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { newState } from "./reactivity-state";
+import { newState, createEffect } from "./reactivity-state";
 test("create a state", () => {
   const personState = { name: "Carlos", age: 26 };
   const state = newState(personState);
@@ -7,4 +7,18 @@ test("create a state", () => {
   expect(state.name).toEqual(personState.name);
   expect(state.age).toEqual(personState.age);
   expect(state.genre).toEqual("male");
+});
+test("test sum reactive sum", () => {
+  const state = newState({});
+  state.a = 1;
+  state.b = 2;
+
+  createEffect(() => {
+    state.sum = state.a + state.b;
+  });
+  expect(state.sum).toEqual(3);
+  state.a = 5;
+  Promise.resolve().then(() => {
+    expect(state.sum).toEqual(7);
+  });
 });
--- src/reactivity-state.ts
+++ src/reactivity-state.ts
@@ -1,11 +1,50 @@
 export function newState(state: { [key: string | symbol]: any }) {
   return new Proxy(state, {
     get(obj, prop) {
+      onGet(prop);
       return obj[prop];
     },
     set(obj, prop, value) {
       obj[prop] = value;
+      onSet(prop, value);
       return true;
     },
   });
+}
+
+const propsToEffects = {} as { [key: string | symbol]: any[] };
+
+let currentEffect: () => void | undefined;
+export function createEffect(effect: () => void) {
+  currentEffect = effect;
+  effect();
+  currentEffect = undefined;
+}
+function onGet(prop: string | symbol) {
+  const effects = propsToEffects[prop] ?? (propsToEffects[prop] = []);
+  effects.push(currentEffect);
+}
+const dirtyEffects = [];
+let queued = false;
+function onSet(prop: string | symbol, value: any) {
+  if (!propsToEffects[prop]) {
+    return;
+  }
+  dirtyEffects.push(...propsToEffects[prop]);
+  if (!queued) {
+    queued = true;
+    queueMicrotask(() => {
+      queued = false;
+      flush();
+    });
+  }
+}
+function flush() {
+  while (dirtyEffects.length) {
+    const effect = dirtyEffects.shift();
+    if (!effect) {
+      continue;
+    }
+    effect();
+  }
 }
